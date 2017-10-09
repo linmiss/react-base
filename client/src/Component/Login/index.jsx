951c96f3da40dec694118e2ce8d2b79d1c1d42b9
Tim <tim.lin@letote.cn>
2017-10-09 10:16:04 +0800

    add decorator

--- client/src/Component/Login/index.jsx
+++ client/src/Component/Login/index.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react'
 import './index.css'
-const Login = () => {
-    return (
-        <div className="login">
-            login
-        </div>
-    );
-};
+import { withRouter } from 'react-router-dom'
 
-export default Login;
+// const Login = () => {
+//   return <div className="login">login</div>
+// }
+@withRouter
+class Login extends Component {
+  render() {
+      console.log(this.props,1111)
+    return <div />
+  }
+}
+
+export default Login
